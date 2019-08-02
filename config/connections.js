0bbb90a2a0813c8d93296648b977f7043dc66a3c
Patricia Correia <correiaph13@gmail.com>
2019-08-02 12:10:41 -0500

    directory and files

---
+++ config/connections.js
@@ -0,0 +1,5 @@
+//set up to connect node to musql and export connection to ORM
+
+
+
+module.exports()
---
+++ server.js
@@ -0,0 +1,6 @@
+// Dependencies
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
