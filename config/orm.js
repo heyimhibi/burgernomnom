3ada84cee05c254a8ba2ef5dd96f626ccc7d4a87
Patricia <correiaph13@gmail.com>
2019-08-02 22:20:19 -0500

    got the server to work locally to populate html with default view

--- config/orm.js
+++ config/orm.js
@@ -2,46 +2,6 @@
 // Import MySQL connection.
 var connection = require("../config/connections.js");
 
-
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
-
-// function printQuestionMarks(num) {
-//   var arr = [];
-
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-
-//   return arr.toString();
-// }
-
-// // Helper function to convert object key/value pairs to SQL syntax
-// function objToSql(ob) {
-//   var arr = [];
-
-//   // loop through the keys and push the key/value as a string int arr
-//   for (var burger_name in ob) {
-//     var value = ob[burger_name];
-//     // check to skip hidden properties
-//     if (Object.hasOwnProperty.call(ob, burger_name)) {
-//       // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = "'" + value + "'";
-//       }
-//       // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//       // e.g. {sleepy: true} => ["sleepy=true"]
-//       arr.push(burger_name + "=" + value);
-//     }
-//   }
-
-//   // translate array of strings to a single comma-separated string
-//   return arr.toString();
-// }
-
 // Object for all our SQL statement functions.
 var orm = {
   all: function(tableInput, cb) {
@@ -52,21 +12,33 @@ var orm = {
       }
       cb(result);
   });
-
 },
-};
-console.log ("test-orm");
+create: function(tableInput,  cb){
+  var queryString = ("INSERT INTO burgers_table (buger_name) VALUES ('new')");
+  connection.query(queryString, function(err, result) {
+    if (err) {
+      throw err;
+    }
+    cb(result);
+  });
+  
+  console.log (queryString);
+},
+}
+// console.log ("test-orm");
+// console.log(queryString);
+// console.log(burgers_table);
 
 
-//     // var queryString = "SELECT * FROM " + currentburgers + ";";
-//     connection.query(queryString, function(err, result) {
-//       if (err) {
-//         throw err;
-//       }
-//       // cb(result);
-//       console.log(queryString);
-//     };
-//   });
+    // var queryString = "SELECT * FROM " + currentburgers + ";";
+    // connection.query(queryString, function(err, result) {
+    //   if (err) {
+    //     throw err;
+    //   }
+    //   cb(result);
+      // console.log(queryString);
+    // };
+  // });
 // };
   // },
   // create: function(burgers_table, cols, vals, cb) {
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
-
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
 
@@ -9,19 +8,27 @@ router.get("/", function(req, res) {
         var hbsObject = {
           burgers_table: data
         };
-        console.log(hbsObject);
+        // console.log(hbsObject);
         res.render("index", hbsObject); 
         
       });
       
       });
-    
+
+      router.post("/api/burgeradded", function(req, res) {
+        burger.create([
+          "burger_name", "devour"]),
+         
+          [
+            req.body.burger_name, req.body.devour
+          ], function(result) {
+            // Send back the ID of the new quote
+            res.json({ id: result.insertId });
+          console.log(result);
+        };
+      });
       console.log("test-controller");
-      
-    //   // 
-    //   res.render("index");
-    //   
-    // });
+
     
   // Export routes for server.js to use.
   module.exports = router;
